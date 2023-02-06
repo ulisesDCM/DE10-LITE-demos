f83ac2c94485e94d69d5fd47e1c0e520ae666752
ulisesDCM <ulises.cmorales@hotmail.com>
2023-02-05 18:30:39 -0800

    Increased the counter from 999 to 9999.

--- segmentCounter/simpleCounter.v
+++ segmentCounter/simpleCounter.v
@@ -4,12 +4,15 @@ module counter
 	input 	i_rst,
 	output [3:0] o_units,
 	output [3:0] o_tens,
+	output [3:0] o_hundreds,
 	output [3:0] o_thousands
 );
-
+	
+	//Variable declarations
 	reg [32:0]r_tmp_counter = 0;
 	reg [3:0] r_units = 0;
 	reg [3:0] r_tens = 0;
+	reg [3:0] r_hundreds = 0;
 	reg [3:0] r_thousands = 0;
 	
 	//This 'always' block is executed every 100 ns
@@ -33,13 +36,21 @@ module counter
 					if(r_tens == (10-1))
 					begin
 						r_tens <= 0;
-						if(r_thousands == (10-1))
+						if(r_hundreds == (10-1))
 						begin
-							r_thousands <= 0;
+							r_hundreds <= 0;
+							if(r_thousands == (10-1))
+							begin
+								r_thousands <= 0;
+							end
+							else
+							begin
+								r_thousands <= r_thousands + 1;
+							end
 						end
 						else
 						begin
-							r_thousands <= r_thousands+1;
+							r_hundreds <= r_hundreds+1;
 						end
 					end
 					else
@@ -62,4 +73,6 @@ module counter
 	assign o_units = r_units;
 	assign o_tens  = r_tens;
 	assign o_thousands = r_thousands;
+	assign o_hundreds = r_hundreds;
+
 endmodule
