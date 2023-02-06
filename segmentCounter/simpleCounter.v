51318305217dc4946493c8ec1aefffc66f6d629f
ulisesDCM <ulises.cmorales@hotmail.com>
2023-02-05 17:21:50 -0800

    fixing the problem with the counter module. The temporary register counter had the wrong size of 1 bit.

--- segmentCounter/simpleCounter.v
+++ segmentCounter/simpleCounter.v
@@ -7,8 +7,43 @@ module counter
 	output [3:0] o_thousands
 );
 
+	reg [32:0]r_tmp_counter = 0;
+	reg [3:0] r_units = 0;
+	reg [3:0] r_tens = 0;
+	reg [3:0] r_thousands = 0;
+	
+	//This 'always' block is executed every 100 ns
 	always @(posedge i_clk)
 	begin
+		if(i_rst == 1)
+		begin
+			r_tmp_counter <= 0;
+			r_units <= 0;
+			r_tens <= 0;
+			r_thousands <= 0;
+		end
+		else
+		begin
+			if(r_tmp_counter == (100))
+			begin
+				r_tmp_counter <= 0;
+				if(r_units == 9)
+				begin
+					r_units <= 0;
+				end
+				else
+				begin
+					r_units <= r_units +1;
+				end
+			end
+			else
+			begin
+				r_tmp_counter <= r_tmp_counter + 1;
+			end
+		end		
 	end
 	
+	assign o_units = r_units;
+	assign o_tens  = r_tens;
+	assign o_thousands = r_thousands;
 endmodule
